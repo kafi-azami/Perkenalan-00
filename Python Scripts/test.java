0dc9969e1d35f6871c4e38765ac9a477d9899145
kafi-azami <kafi_ajuna_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:53:33 +0700

    Add files via upload

---
+++ Python Scripts/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Nama: Kafi Azami Ajuna");
+        System.out.println("Alamat: Dsn. Bacem RT 004 RW 002 Ds. Bacem Kec. Ponggok Kab. Blitar");
+        System.out.println("SMP: SMPIT IBADURRAHMAN");
+        System.out.println("NO.HP: 081259799059");
+    }
+}
